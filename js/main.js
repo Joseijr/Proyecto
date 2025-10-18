da4222c233551b68adf62d1eb21dad7c52306544
Joseijr <jjimenezrubi6@gmail.com>
2025-10-18 17:05:35 -0600

    Pagina base

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 const app = Vue.createApp({
 
 
-data(){
-    return{
+    data() {
+        return {
+
+            image: "../assets/Illustrationproyecto1.jpg",
+            variants: [
+                { id: 1, image: "../assets/Illustrationproyecto1.jpg" },
+                { id: 2, image: "../assets/Illustrationproyecto2.jpg" },
+                { id: 3, image: "../assets/Illustrationproyecto3.jpg" },
+            ]
+        };
+    },
+
+    methods: {
+        showImage(variant) {
+            this.selectedImage = variant.image;
+            console.log(variant.id);
+
+
+        },
 
     }
-}
-
-/*methods:{
-
-}
-*/
 });
