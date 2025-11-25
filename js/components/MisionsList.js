93b2d9bd62ea7ee903bddfb6809bca866d78e4da
Joseijr <163027334+Joseijr@users.noreply.github.com>
2025-11-24 19:30:28 -0600

    Lista de misiones

---
+++ js/components/MisionsList.js
@@ -0,0 +1,52 @@
+app.component('missions-list', {
+    data() {
+        return {
+            missions: [],
+            loading: false,
+            error: null
+        };
+    },
+    methods: {
+        getMissions() {
+            this.loading = true;
+            this.error = null;
+            const server = 'http://prueba.test';
+            fetch(server + '/api/v1/missions') // Endpoint de tus misiones
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    this.missions = data;
+                    this.loading = false;
+                })
+                .catch(error => {
+                    this.error = error.message;
+                    this.loading = false;
+                });
+        }
+    },
+    created() {
+        this.getMissions();
+    },
+    template: /*html*/`
+    <div>
+      <h2>Misiones</h2>
+      <div v-if="loading">Cargando misiones...</div>
+      <div v-if="error" style="color: red">{{ error }}</div>
+      <ul v-if="missions.length">
+        <li v-for="mission in missions" :key="mission.id">
+          <p class="extra-color text-xxl">{{ mission.title }}</p>
+          <span>{{ mission.description }}</span><br>
+          <p>Recompensa: <span class="extra-color">{{ mission.reward }}</span> monedas</p>
+          <button>Reclamar</button>
+          <hr>
+        </li>
+      </ul>
+      
+      <div v-else-if="!loading">No hay misiones disponibles.</div>
+    </div>
+  `
+});
